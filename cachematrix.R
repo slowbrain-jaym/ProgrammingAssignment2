9b0f56ae2fec9a68a7dc0b01894d021aa7552701
slowbrain-jaym <jamenator94@gmail.com>
2020-06-08 14:45:51 +0100

    Completed Stub Functions in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<- NULL
+  set<-function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inversematrix) inv <<-inversematrix
+  getinv <- function() inv
+  list(set = set, get = get, setinv=setinv, getinv=getinv)
 }
 
 
@@ -12,4 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
+
